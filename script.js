fca8fbeee3d73b5b97bf044fdc1d00a3fc58e763
Kamruzzaman Nazmul <64187721+0Himu1@users.noreply.github.com>
2021-12-23 15:49:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ class Calculator {
         computation = prev * current;
         break;
       case "÷":
-        computation = prev - current;
+        computation = prev / current;
         break;
       default:
         return;
